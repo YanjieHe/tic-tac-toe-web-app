4498d651e2f9864d53cbe8de2ee6d3a61916fcb8
heyanjie <heyanjie0@outlook.com>
2018-12-28 10:13:26 -0500

    Updates

---
+++ precache-manifest.d36f443b667bfcb60f3617cc517a05d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43b452a8a3662c822ca4",
+    "url": "/tic-tac-toe-web-app/static/css/main.c93da294.chunk.css"
+  },
+  {
+    "revision": "43b452a8a3662c822ca4",
+    "url": "/tic-tac-toe-web-app/static/js/main.43b452a8.chunk.js"
+  },
+  {
+    "revision": "1860a0ef601cbac979b7",
+    "url": "/tic-tac-toe-web-app/static/js/1.1860a0ef.chunk.js"
+  },
+  {
+    "revision": "bcd4ca21fb7b78162b54",
+    "url": "/tic-tac-toe-web-app/static/js/runtime~main.bcd4ca21.js"
+  },
+  {
+    "revision": "bc0167babb2324d741017b4e25e2e4ff",
+    "url": "/tic-tac-toe-web-app/index.html"
+  }
+];
